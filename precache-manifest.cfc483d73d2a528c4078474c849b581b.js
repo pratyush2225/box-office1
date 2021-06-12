492ac40c6655f3cf4921e1683cc747f5d975e98b
Ricky <pratyush.ricky10@gmail.com>
2021-06-12 20:18:06 +0530

    Updates

---
+++ precache-manifest.cfc483d73d2a528c4078474c849b581b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00073b770c156d120fd2dbb4e87251ab",
+    "url": "/box-office1/index.html"
+  },
+  {
+    "revision": "15643122dd55d32aadcd",
+    "url": "/box-office1/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "eaa36513363c63364a97",
+    "url": "/box-office1/static/js/2.04f148ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office1/static/js/2.04f148ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15643122dd55d32aadcd",
+    "url": "/box-office1/static/js/main.39d1a2d6.chunk.js"
+  },
+  {
+    "revision": "c4922a5f00893a7c3e11",
+    "url": "/box-office1/static/js/runtime-main.93c18862.js"
+  }
+]);
